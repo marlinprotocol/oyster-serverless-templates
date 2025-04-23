868c1d8c3253d979698cf22451da5518df55e63e
Sagar Parker <32793769+sagarparker@users.noreply.github.com>
2025-04-23 14:33:36 +0530

    Update worker.js

--- factorials-of-a-number/worker.js
+++ factorials-of-a-number/worker.js
@@ -1,35 +1,32 @@
-addEventListener('fetch', event => {
-    event.respondWith(handle(event.request));
-});
+export default {
+    async fetch(request) {
+        try {
+            const data = await request.json();
 
-async function handle(request) {
-    try {
-        const data = await request.json();
-        console.log(data);
-
-        let num = data.num;
-        let factors = [];
-        let divisor = 2;
-        if(typeof num != 'number') {
-            return new Response(`Please provide a valid integer as input in the format {'num':10}`);
-        }
-
-        while (num > 2) {
-            if (num % divisor === 0) {
-                factors.push(divisor);
-                num /= divisor;
-            } else {
-                divisor++;
+            let num = data.num;
+            let factors = [];
+            let divisor = 2;
+            if(typeof num != 'number') {
+                return new Response(`Please provide a valid integer as input in the format {'num':10}`);
             }
-        }
 
-        if(factors.length == 0) {
-            return new Response('No prime factor for the provided number');
-        }
+            while (num > 2) {
+                if (num % divisor === 0) {
+                    factors.push(divisor);
+                    num /= divisor;
+                } else {
+                    divisor++;
+                }
+            }
 
-        return new Response(factors);
-    } catch(err) {
-        console.log(err);
-        return new Response('Please provide a valid JSON input');
+            if(factors.length == 0) {
+                return new Response('No prime factor for the provided number');
+            }
+
+            return new Response(factors);
+        } catch(err) {
+            console.log(err);
+            return new Response('Please provide a valid JSON input');
+        }
     }
-}
+};
