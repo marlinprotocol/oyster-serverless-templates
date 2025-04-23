3afa18b2aa5c7ea6c6388b9468bafb4597499846
Sagar Parker <32793769+sagarparker@users.noreply.github.com>
2025-04-23 15:19:48 +0530

    Update worker.js

--- eth-price/worker.js
+++ eth-price/worker.js
@@ -1,17 +1,13 @@
-addEventListener(
-    'fetch',
-    event => { 
-        event.respondWith(handle(event.request));
-    });
-
-async function handle(request) {
-    try {
-        const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd');
-        const data = await response.json();
-        const ethPrice = data.ethereum.usd;
-        return new Response(ethPrice);
-    } catch (err) {
-        console.log(err); 
-        return new Response('Error fetching eth price');
+export default {
+    async fetch(request) {
+        try {
+            const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd');
+            const data = await response.json();
+            const ethPrice = data.ethereum.usd;
+            return new Response(ethPrice);
+        } catch (err) {
+            console.log(err); 
+            return new Response('Error fetching eth price');
+        }
     }
-}
+};
